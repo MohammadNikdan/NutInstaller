a67818ec60f4a711195640cb994b3bdddfefdd63
MohammadNikdan <110032445+MohammadNikdan@users.noreply.github.com>
2026-08-14 21:13:49 +0330

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Nutricula Expert Advisor Installer")]
+[assembly: AssemblyDescription("Nutricula Expert Advisor installer and license activation client")]
+[assembly: AssemblyCompany("Highflyers")]
+[assembly: AssemblyProduct("Nutricula Expert Advisor Installer")]
+[assembly: AssemblyCopyright("Copyright © Highflyers")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
